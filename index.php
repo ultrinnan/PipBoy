78397c4faf63332ac5412a97594a8cc8d5e5a04a
Serhii Fedirko <sergey.fedirko@gmail.com>
2020-02-27 18:11:20 +0200

    uncomment video tag

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
     </div>
 
     <div class="main">
-<!--        <video poster="/images/standby.jpg" autoplay muted id="video">-->
-<!--            <source src="/startup.mp4" type="video/mp4">-->
-<!--            Your browser does not support HTML5 video.-->
-<!--        </video>-->
+        <video poster="/images/standby.jpg" autoplay muted id="video">
+            <source src="/startup.mp4" type="video/mp4">
+            Your browser does not support HTML5 video.
+        </video>
         <div class="content" id="content">
             <div class="screen-reflection"></div>
             <div class="scan"></div>
