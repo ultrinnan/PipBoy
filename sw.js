888be0b4b9d094ef7f4902258324c7cb54719170
zgs225 <zgs225@gmail.com>
2025-01-25 16:02:01 +0800

    chore: remove comments

--- sw.js
+++ sw.js
@@ -21,20 +21,16 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // 如果缓存中有，直接返回
       if (response) {
         return response;
       }
 
-      // 如果没有缓存，先获取资源
       return fetch(event.request).then((response) => {
-        // 检查资源是否匹配我们的缓存模式
         const url = new URL(event.request.url);
         const shouldCache = CACHE_PATTERNS.some((pattern) =>
           pattern.test(url.pathname),
         );
 
-        // 如果匹配模式，缓存资源
         if (shouldCache) {
           const responseToCache = response.clone();
           caches.open(CACHE_NAME).then((cache) => {
